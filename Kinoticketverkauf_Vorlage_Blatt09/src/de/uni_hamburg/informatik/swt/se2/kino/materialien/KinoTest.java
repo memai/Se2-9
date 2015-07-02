70aa72f545e6d07d87385bc091ca6850685c25df
memai <meike.zimmermann@gmail.com>
2015-07-02 18:10:34 +0200

    Geldbetrag in Kinotest eingefuegt

--- Kinoticketverkauf_Vorlage_Blatt09/src/de/uni_hamburg/informatik/swt/se2/kino/materialien/KinoTest.java
+++ Kinoticketverkauf_Vorlage_Blatt09/src/de/uni_hamburg/informatik/swt/se2/kino/materialien/KinoTest.java
@@ -11,6 +11,7 @@ import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.Datum;
 import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.FSK;
 import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.Platz;
 import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.Uhrzeit;
+import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.Geldbetrag;
 
 public class KinoTest
 {
@@ -34,6 +35,9 @@ public class KinoTest
 
     private Datum _d1;
 
+    private Geldbetrag _900;
+    private Geldbetrag _1000;
+    
     private Vorstellung _vorstellungSaal1Film0;
     private Vorstellung _vorstellungSaal2Film1;
     private Vorstellung _vorstellungSaal2Film2a;
@@ -66,15 +70,18 @@ public class KinoTest
         _u4 = Uhrzeit.get(1, 30);
 
         _d1 = Datum.get(11, 07, 2008);
+        
+        _900 = Geldbetrag.get(900);
+        _1000 = Geldbetrag.get(1000);
 
         _vorstellungSaal1Film0 = new Vorstellung(_saal1, _film0, _u1, _u2, _d1,
-                900);
+                _900);
         _vorstellungSaal2Film1 = new Vorstellung(_saal2, _film1, _u1, _u2, _d1,
-                1000);
+                _1000);
         _vorstellungSaal2Film2a = new Vorstellung(_saal2, _film2, _u2, _u3,
-                _d1, 900);
+                _d1, _900);
         _vorstellungSaal2Film2b = new Vorstellung(_saal2, _film2, _u3, _u4,
-                _d1, 900);
+                _d1, _900);
 
         _alleVorstellungen = new Vorstellung[] { _vorstellungSaal1Film0,
                 _vorstellungSaal2Film1, _vorstellungSaal2Film2a,
