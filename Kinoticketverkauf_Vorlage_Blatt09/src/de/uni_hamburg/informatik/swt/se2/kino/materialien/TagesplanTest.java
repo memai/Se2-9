d08a5fc33a6af88a57d08e1a322c50179e9f12a6
memai <meike.zimmermann@gmail.com>
2015-07-02 18:12:41 +0200

    Geldbetrag in TagesplanTest eingefuegt

--- Kinoticketverkauf_Vorlage_Blatt09/src/de/uni_hamburg/informatik/swt/se2/kino/materialien/TagesplanTest.java
+++ Kinoticketverkauf_Vorlage_Blatt09/src/de/uni_hamburg/informatik/swt/se2/kino/materialien/TagesplanTest.java
@@ -7,10 +7,12 @@ import org.junit.Test;
 import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.Datum;
 import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.FSK;
 import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.Uhrzeit;
+import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.Geldbetrag;
 
 public class TagesplanTest  //TODO Blatt9 Fachwert Geldbetrag Testklasse Tagesplan
 {
     private static final Datum _datum = Datum.get(1, 1, 2010);
+    private static final Geldbetrag _geldbetrag = Geldbetrag.get(0);
     private static final Film _film = new Film("", 1, FSK.FSK0, false);
     private static final Kinosaal _kinosaal = new Kinosaal("Saal 1", 1, 1);
     private static final Kinosaal _kinosaal2 = new Kinosaal("Saal 2", 1, 1);
@@ -18,11 +20,11 @@ public class TagesplanTest  //TODO Blatt9 Fachwert Geldbetrag Testklasse Tagespl
     private static final Uhrzeit _startzeit2 = Uhrzeit.get(0, 1);
     private static final Uhrzeit _endzeit = Uhrzeit.get(1, 1);
     private static final Vorstellung _vorstellung = new Vorstellung(_kinosaal,
-            _film, _startzeit, _endzeit, _datum, 0); 
+            _film, _startzeit, _endzeit, _datum, _geldbetrag); 
     private static final Vorstellung _vorstellung2 = new Vorstellung(_kinosaal,
-            _film, _startzeit2, _endzeit, _datum, 0);
+            _film, _startzeit2, _endzeit, _datum, _geldbetrag);
     private static final Vorstellung _vorstellung3 = new Vorstellung(
-            _kinosaal2, _film, _startzeit2, _endzeit, _datum, 0);
+            _kinosaal2, _film, _startzeit2, _endzeit, _datum, _geldbetrag);
 
     private Tagesplan _t;
 
